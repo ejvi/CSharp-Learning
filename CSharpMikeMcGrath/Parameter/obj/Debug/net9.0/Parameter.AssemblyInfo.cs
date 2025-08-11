ee985b7a19af6fe715a1d706ee969290f1a48f75
ejvi <evolutionsup@gmail.com>
2025-08-12 02:42:03 +0300

    new folder

--- CSharpMikeMcGrath/Parameter/obj/Debug/net9.0/Parameter.AssemblyInfo.cs
+++ CSharpMikeMcGrath/Parameter/obj/Debug/net9.0/Parameter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parameter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22af2bff97c5919181aa4ed1709d4fd6517e65de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7893611aa52a6ad4506284ce4b4523c42cad1f55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parameter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parameter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
