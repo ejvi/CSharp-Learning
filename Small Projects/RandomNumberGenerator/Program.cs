9922d8d3117e3ca8fbe485ca727dc793a1f9f3b8
ejvi <evolutionsup@gmail.com>
2025-08-12 02:41:53 +0300

    new folder

---
+++ Small Projects/RandomNumberGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿public class RandomNumberGenerator
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random(); // Create a single instance
+
+        Console.WriteLine("Generating 5 random numbers:");
+        for (int i = 0; i < 5; i++)
+        {
+            int num = random.Next(1, 999); // Numbers between 1 and 100
+            Console.WriteLine($"Random number {i + 1}: {num}");
+        }
+    }
+}
