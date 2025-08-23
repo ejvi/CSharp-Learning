90289ab6ed15d818860d6aa54b49d16aef4afa8f
ejvi <evolutionsup@gmail.com>
2025-08-23 04:35:58 +0300

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿int max = 100;
+
+for (int i = 1; i <= max; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
